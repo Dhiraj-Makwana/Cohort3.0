7b294d4b75e0532ff317a494d26118b9d0848d50
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-02 00:08:24 +0530

    add DataBase logic

--- Todo_app_mongoDB/index.js
+++ Todo_app_mongoDB/index.js
@@ -1,13 +1,52 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "World$Be$tProgrammer";
+const mongoose = require("mongoose");
+const { UserModel, TodoModel } = require("./db");
 
+mongoose.connect("mongodb+srv://mrincident265_db_user:poAdMa4BPIiRgJfN@cluster0.jl6xp7n.mongodb.net/todo-test")
 const app = express();
+app.use(express.json());
 
-app.post("/signup", function(req, res) {
+app.post("/signup", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    });
+
+    res.json({
+        message: "You are signed up successfully"
+    });
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if(user) {
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        });
+    }
+    else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        });
+    }
 });
 
 app.post("/todo", function(req, res) {
